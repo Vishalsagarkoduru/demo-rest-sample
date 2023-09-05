818c7ec999eab47c1cf477a6b93593acb745d03c
Vishalsagarkoduru <135941640+Vishalsagarkoduru@users.noreply.github.com>
2023-09-05 17:18:51 +0530

    Update DemoController.java

--- src/main/java/com/example/demosample/DemoController.java
+++ src/main/java/com/example/demosample/DemoController.java
@@ -11,17 +11,17 @@ public class DemoController {
     Logger logger = (Logger) LoggerFactory.getLogger(DemoController.class);
     @GetMapping("/printmsg")
     public String pringMsg(){
-        for (int i=0;i<10000;i++){
+        for (int i=0;i<1000;i++){
+           logger.info("=Hello World=");
+           logger.info("==Hello World==");
+           logger.info("===Hello World====");
+           logger.info("====Hello World=====");
+           logger.info("=====Hello World======");
+           logger.info("======Hello World=======");
+           logger.info("=======Hello World========");
+           logger.info("========Hello World=========");
            logger.info("=========Hello World==========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World==========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
-           logger.info("=========Hello World===========");
+           logger.info("=========+Hello World===========");
         }
 return "hello World\n hello World\n hello World\n hello World\n"; 
     }
